8a496e9617ce24075c3904db7f48b76d99509d47
Animesh <animesh@Animeshs-MacBook-Pro.local>
2024-03-31 14:33:35 +0530

    ep-01

---
+++ App.js
@@ -0,0 +1,14 @@
+
+// Create a nested html
+//const parent = React.createElement("div", {id: "parent"}, React.createElement("div", {id: "child"}, React.createElement("h1", {}, "I'm an H1 tag")))
+
+// Create nested HTML with siblings
+const parent = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, [React.createElement("h1", {}, "I'm an H1 tag"), React.createElement("h2", {}, "I'm an H2 tag")]))
+
+
+const heading = React.createElement("h1", { id: "heading" }, "Hello world from React");
+console.log(parent) // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent)
